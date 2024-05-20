9b4004e22d4050684c952929f7c1bb7f880f0dc1
Prechak <benedict.precha@gmail.com>
2024-05-20 11:29:42 +0700

    (ex-5): use filter to filter obj

--- ex-4.js
+++ ex-4.js
@@ -8,9 +8,12 @@ const todos = [
 
 function getCompletedTodo(todos) {
   // Start coding here
+  return todos.filter(function (todo) {
+    return todo.completed === true;
+  });
 }
 
-getCompletedTodo(todos);
+console.log(getCompletedTodo(todos));
 
 /* 
 	Output:
